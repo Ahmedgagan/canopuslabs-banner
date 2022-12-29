4e4b87ead5f6caff6bb5d52ba61b9e1bc11cc7eb
Ahmed Gagan <ahmedgagan6@gmail.com>
2022-12-29 20:33:32 +0530

    first commit

---
+++ javascripts/discourse/components/canopus-labs-banner.js
@@ -0,0 +1,27 @@
+import Component from "@ember/component";
+import discourseComputed from "discourse-common/utils/decorators";
+import { inject as service } from "@ember/service";
+import { computed } from "@ember/object";
+import { defaultHomepage } from "discourse/lib/utilities";
+export default Component.extend({
+  router: service(),
+
+  bannerLinks: computed(function () {
+    return JSON.parse(settings.banner_links);
+  }),
+
+  @discourseComputed("router.currentRouteName", "router.currentURL")
+  showHere(currentRouteName, currentURL) {
+    if (settings.show_on === "all") {
+      return true;
+    }
+
+    if (settings.show_on === "discovery") {
+      return currentRouteName.indexOf("discovery") > -1;
+    }
+
+    if (settings.show_on === "homepage") {
+      return currentRouteName == `discovery.${defaultHomepage()}`;
+    }
+  },
+});
